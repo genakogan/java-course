7e9a4202f109dc56daefddf424cd0f8c32a6743c
GenadyKogan <rgkogan@gmail.com>
2023-12-03 14:25:54 +0200

    default constructor

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,32 +1,49 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        // if - else example
+//        Examples.ifElseExanple();
+//
+//        // switch - case Example
+//        Examples.switchCaseExample();
+//
+//        // try - catch example
+//        Examples.tryCatchExample();
+//
+//        // for loop example
+//        Examples.forLoopExample();
+//
+//        // while loop example
+//        Examples.whileLoopExample();
+//
+//        //  do - while loop example
+//        Examples.doWhileLoopExample();
+//
+//        // map example
+//        Examples.mapExample();
+//
+//        // solution task 9
+//        Examples.solutionTask9();
+//
+//        scanner.close();
+//    }
 
-        // if - else example
-        Examples.ifElseExanple();
+    private int value;
 
-        // switch - case Example
-        Examples.switchCaseExample();
-
-        // try - catch example
-        Examples.tryCatchExample();
-
-        // for loop example
-        Examples.forLoopExample();
-
-        // while loop example
-        Examples.whileLoopExample();
-
-        //  do - while loop example
-        Examples.doWhileLoopExample();
-
-        // map example
-        Examples.mapExample();
-
-        // solution task 9
-        Examples.solutionTask9();
-
-        scanner.close();
+    // Дефолтный конструктор
+    public Main() {
+        this.value = 9;
     }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Main example = new Main();
+        System.out.println("Value: " + example.getValue());
+    }
+
 }
