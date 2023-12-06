c6b82a5165c9d1f22f24a9a8437be9581e8fa81d
GenadyKogan <rgkogan@gmail.com>
2023-12-06 17:06:34 +0200

    Person example

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,49 +1,11 @@
 import java.util.Scanner;
 public class Main {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        // if - else example
-//        Examples.ifElseExanple();
-//
-//        // switch - case Example
-//        Examples.switchCaseExample();
-//
-//        // try - catch example
-//        Examples.tryCatchExample();
-//
-//        // for loop example
-//        Examples.forLoopExample();
-//
-//        // while loop example
-//        Examples.whileLoopExample();
-//
-//        //  do - while loop example
-//        Examples.doWhileLoopExample();
-//
-//        // map example
-//        Examples.mapExample();
-//
-//        // solution task 9
-//        Examples.solutionTask9();
-//
-//        scanner.close();
-//    }
 
-    private int value;
-
-    // Дефолтный конструктор
-    public Main() {
-        this.value = 9;
-    }
-
-    public int getValue() {
-        return value;
-    }
 
     public static void main(String[] args) {
-        Main example = new Main();
-        System.out.println("Value: " + example.getValue());
+        Person example1 = new Person("fdffsdf",25);
+        Person example2 = new Person(new Person("asdasd",example1.getAge()));
+        System.out.println("Value: " + example2.getAge());
     }
 
 }
